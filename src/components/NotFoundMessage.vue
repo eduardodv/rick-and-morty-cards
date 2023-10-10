9db58fb7b6f3b7a5a54357c89ebfa551b083fb3f
Eduardo Dalla Vecchia <eduardo@MacBook-Pro-de-Eduardo.local>
2023-10-10 16:32:53 -0300

    Filter functionality

--- src/components/NotFoundMessage.vue
+++ src/components/NotFoundMessage.vue
@@ -1,14 +1,28 @@
 <template>
   <section>
     <h3>Ops, no characters found!</h3>
+    <ul>
+      <li>The terms used for the search were not found;</li>
+      <li>Try changing the filter.</li>
+    </ul>
   </section>
 </template>
 
 <style lang="scss" scoped>
+section {
+  padding: 3rem 0;
+  text-align: center;
+
   h3 {
-    text-align: center;
-    font-size: 2rem;
-    padding: 3rem 0;
+    font-size: 1.5rem;
     font-weight: bold;
+    margin-bottom: 1rem;
   }
+
+  ul {
+    display: inline-block;
+    text-align: left;
+    padding: 0;
+  }
+}
 </style>
--- src/stores/characterStore.ts
+++ src/stores/characterStore.ts
@@ -7,20 +7,24 @@ export const useCharacterStore = defineStore('character', () => {
   const isLoading = ref(false)
   const characters = ref([])
   const pageInfo = ref({})
-  const episodes:any = ref([])
+  const episodes = ref<string[]>([]);
+  const queryStore = ref('')
+  const statusStore = ref('')
 
-  const fetchCharacters = async (query?: string) => {
+  const fetchCharacters = async (query: string = '') => {
     isLoading.value = true
 
+    queryStore.value = query
+    
     // await new Promise((r) => setTimeout(r, 2000)) // Fake promise for delay
 
-    await api.get(`/character${query ? '?name='+query : '' }`)
+    await api.get(`/character?name=${queryStore.value}${statusStore.value ? '&status=' + statusStore.value : ''}`)
     .then((response) => {
       characters.value = response.data.results
       pageInfo.value = response.data.info
       
       isLoading.value = false
-
+      
       const listEpisodes = response.data.results;
 
       const promises = listEpisodes.map((rota : { episode: string[] }) => {
@@ -46,24 +50,18 @@ export const useCharacterStore = defineStore('character', () => {
     })
   }
 
-  // const searchCharacter = async (query: string) => {
-  //   await api.get(`/character?name=${query}`)
-  //   .then((response) => {
-  //     characters.value = response.data.results
-  //     pageInfo.value = response.data.info
-  //   }).catch((err) => {
-  //     console.log(err);
-  //     characters.value = []
-  //     pageInfo.value = {}
-  //   })
-  // }
+  const filterByStatus = async (status: string) => {
+    statusStore.value = status
+    fetchCharacters(queryStore.value)
+  }
 
   return { 
     isLoading,
     characters,
     pageInfo,
     episodes,
+    statusStore,
     fetchCharacters,
-    // searchCharacter
+    filterByStatus
   }
 })
